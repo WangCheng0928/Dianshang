5f77159e8eb9d9c09b7eadbf9d7006d82444a1a0
WangCheng0928 <493475850@qq.com>
2018-05-15 16:30:01 +0800

    token_

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -8,7 +8,7 @@ public class Const {
     public static final String CURRENT_USER="currentUser";
     public static final String EMAIL="email";
     public static final String USERNAME="username";
-    public static final String TOKEN_PREFIX="_token";
+    public static final String TOKEN_PREFIX="token_";
     public interface ProductListOrderBy{
         Set<String> PRICE_ASC_DESC= Sets.newHashSet("price_desc","price_asc");
     }
