5e9cfd29e04853f328447e799f13b6e78233c707
WangCheng0928 <493475850@qq.com>
2018-05-19 11:08:19 +0800

    commit springsession

--- src/main/java/com/mmall/pojo/User.java
+++ src/main/java/com/mmall/pojo/User.java
@@ -1,8 +1,10 @@
 package com.mmall.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class User {
+
+public class User implements Serializable {
     private Integer id;
 
     private String username;
